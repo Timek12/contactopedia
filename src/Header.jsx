72e41c690a4928424be2cbba3da05ad71d36b44c
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-04 18:58:24 +0100

    Refactor project file structure, append components from previous countopedia project

---
+++ src/Header.jsx
@@ -0,0 +1,31 @@
+import logo from "./images/react.png"
+function MainHeader() {
+  return(
+    <div className="pt-3 py-1 pl-2" style={{backgroundColor: "black"}}>
+      <img alt= "" src={logo} style = {{ height: "35px", verticalAlign: "top"}}>
+      </img>
+      <span className="h2 pt-4 text-white-50">Contactopedia</span>
+    </div>
+  );
+}
+
+const subHeaderStyle = {
+  color: "blueviolet",
+  backgroundColor: "lightgrey",
+}
+
+function SubHeader(){
+  return <p style={subHeaderStyle}>This is an exciting course!</p>;
+}
+
+const Header = () => {
+  return (
+    <div>
+      <MainHeader />
+      <SubHeader />
+    </div>
+  );
+}
+
+export default Header;
+
