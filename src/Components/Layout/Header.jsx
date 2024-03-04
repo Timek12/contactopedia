f4c8a616a3afea3459eb5928f67a117fde89079d
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-04 21:38:56 +0100

    Add all necessary component files, implement footer component

--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,10 +1,10 @@
-import logo from "./images/react.png"
+import logo from "../../images/react.png"
 function MainHeader() {
   return(
     <div className="pt-3 py-1 pl-2" style={{backgroundColor: "black"}}>
       <img alt= "" src={logo} style = {{ height: "35px", verticalAlign: "top"}}>
       </img>
-      <span className="h2 pt-4 text-white-50">Contactopedia</span>
+      <span className="h2 pt-4 text-white-50">ContactOpedia</span>
     </div>
   );
 }
@@ -15,7 +15,7 @@ const subHeaderStyle = {
 }
 
 function SubHeader(){
-  return <p style={subHeaderStyle}>This is an exciting course!</p>;
+  return <p style={subHeaderStyle}></p>;
 }
 
 const Header = () => {
