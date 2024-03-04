f8b64fccd88d24f988f49f1d6be22b91eba54dfb
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-04 21:42:24 +0100

    Add general contact button

---
+++ src/Components/ContactPages/GeneralContact.jsx
@@ -0,0 +1,11 @@
+const GeneralContact = () => {
+    return (
+        <div>
+            <button className="btn btn-secondary form-control">
+                General Contact
+            </button>
+        </div>
+    );
+};
+
+export default GeneralContact;
